f1739447adb27d220d5545f424ef925f0c2c3726
Mayankd21 <110493499+Mayank-Digarse@users.noreply.github.com>
2024-01-02 13:52:30 +0000

    Projects Pushes 2

---
+++ 07_projects/02_bmi_calculator/chaiaurcode.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form');
+//height aur weight ko andar likho
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const result = parseInt(document.querySelector('#results'));
+
+  if (height === '' || height < 0 || isNaN(height)){
+    result.innerHTML = ` Please give a valid height ${height}`;
+  }
+   else if (weight === '' || weight < 0 || isNaN(weight)){
+    result.innerHTML = ` Please give a valid weight ${weight}`;
+  }
+  else{
+   const bmi = (weight/((height*height)/1000)).toFixed(2)
+
+   //show the result
+   result.innerHTML = `<span>${bmi}</span>`;
+  }
+  
+});
